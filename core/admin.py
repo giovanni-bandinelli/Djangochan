8abfd17ece02a57064bcdec1e1414fcbd39fab77
Giovanni <giovabandi02>
2023-11-16 21:29:35 +0100

    1st draft of core models & respective admin class

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from core.models import *
+
+class BoardAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Board, BoardAdmin)
+
+class ThreadAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Thread, ThreadAdmin)
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post, PostAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Board(models.Model):
+    name = models.CharField(max_length=30, unique=True)
+    description = models.TextField()
+    def __str__(self):
+        return self.name
+
+class Thread(models.Model):
+    board = models.ForeignKey(Board, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    username = models.CharField(max_length=50, default='anonymous')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    #is_pinned = models.BooleanField(default=False)
+    def __str__(self):
+        return f"{self.title} - {self.created_at}"
+
+  
+class Post(models.Model):
+    thread = models.ForeignKey(Thread, on_delete=models.CASCADE)
+    username = models.CharField(max_length=255, default='anonymous')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+   # is_op = models.BooleanField(default=False)
+    def __str__(self):
+        return f"[{self.thread.title}] {self.created_at} "
+
+    
+
+
+
