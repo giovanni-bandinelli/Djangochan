f54cf4cca8fb5c1712c3f0ca67723cdf3d4ef94e
Giovanni <giovabandi02>
2023-11-19 22:23:32 +0100

    modified board_page to also render board list

--- core/views.py
+++ core/views.py
@@ -7,13 +7,15 @@ def home(request): #view that returns a list of all the communities (Boards) pre
     return render(request, 'core/home.html', {'boards':boards})
 
 def board_page(request, board_name): #view to access a boards's content
+    boards = Board.objects.all()
     board = Board.objects.get(name=board_name)
     threads = Thread.objects.filter(board=board)
     context = {
+        'boards': boards, #used to let user go on another board through navbar
         'board': board,
         'threads': threads,
     }
-    return render(request, 'core/board_page.html', context)
+    return render(request, 'core/default_board_page.html', context)
 
 def single_thread(request, thread_id): #view page with single thread (used for catalog mode)
     thread = Thread.objects.get(id=thread_id)
