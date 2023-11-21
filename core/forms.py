87c310243ac768f9a4ccee2ab9697791683bfa96
Giovanni <giovabandi02>
2023-11-21 17:34:43 +0100

    first dratf working on form implementation

---
+++ core/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Thread, Post
+
+class ThreadForm(forms.ModelForm):
+    password_delete = forms.CharField(widget=forms.PasswordInput, required=False)
+    class Meta:
+        model = Thread
+        fields = ['title', 'content', 'username', 'password_delete', 'file_uploaded']
+
+
+class PostForm(forms.ModelForm):
+    password_delete = forms.CharField(widget=forms.PasswordInput, required=False)
+    class Meta:
+        model = Thread
+        fields = ['content', 'username', 'password_delete', 'file_uploaded']
--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 # views.py
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Board,Thread,Post
+from .forms import ThreadForm
 
 def home(request): #view that returns a list of all the communities (Boards) present in the Website
     boards = Board.objects.all()
@@ -22,3 +23,15 @@ def single_thread(request, thread_id): #view page with single thread (used for c
     posts = Post.objects.get(thread=thread)
 
 
+def create_thread(request):
+    if request.method == 'POST':
+        form = ThreadForm(request.POST, request.FILES)
+        if form.is_valid():
+            # Save the thread
+            new_thread = form.save(commit=False)
+            new_thread.save()
+            return redirect('your_redirect_url')  # Redirect to the appropriate page
+    else:
+        form = ThreadForm()
+
+    return render(request, 'your_template.html', {'form': form})
