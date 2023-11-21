132252238ab7b6a6dce02b77f6666832b85b7691
Giovanni <giovabandi02>
2023-11-21 17:31:11 +0100

    minor change upload dir name

--- core/models.py
+++ core/models.py
@@ -13,7 +13,7 @@ class Thread(models.Model):
     username = models.CharField(max_length=50, default='anonymous')
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    file_uploaded = models.FileField(upload_to='thread_file', null=True, blank=True)
+    file_uploaded = models.FileField(upload_to='thread_media', null=True, blank=True)
     #is_pinned = models.BooleanField(default=False)
     def __str__(self):
         return f"{self.title} - {self.created_at}"
@@ -24,7 +24,7 @@ class Post(models.Model):
     username = models.CharField(max_length=255, default='anonymous')
     content = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
-    file_uploaded = models.FileField(upload_to='file_uploads/', null=True, blank=True)
+    file_uploaded = models.FileField(upload_to='post_media/', null=True, blank=True)
    # is_op = models.BooleanField(default=False)
     def __str__(self):
         return f"[{self.thread.title}] {self.created_at} "
