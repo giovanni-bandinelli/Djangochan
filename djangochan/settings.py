1079222783f9494db814444ba085f64d43f08c89
giovanni <giovabandi02@gmail.com>
2023-12-22 12:18:04 +0100

    Changed dbms from Postgresql to Sqlite3

--- djangochan/settings.py
+++ djangochan/settings.py
@@ -77,13 +77,9 @@ WSGI_APPLICATION = 'djangochan.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-  'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangochandb',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '',
-        'PORT': '5432'
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
