7d44ea33f833e8fe5c11d415b536c85152e89ae6
giovanni <giovabandi02@gmail.com>
2024-01-10 18:25:24 +0100

    implemented working greentext

--- core/utils.py
+++ core/utils.py
@@ -1,27 +1,26 @@
-import html,re
+import html, re
 
 def markdown_parser(text):
+
     # Escape special characters to avoid html injections
     text = html.escape(text)
 
-    #add greentext markdown with '>text line'(should wrap the whole text line in its span class, '>' has to be the first character in the line for this to work)
-    #text = re.sub(r'(^|\n)>([^\n]*)', r'\1<span class="greentext">&gt;\2</span>', text)
-
-    #go to new line
-    text = text.replace('\n', '<br>')
-
-    #keep track of spaces (perhaps not a good idea?)
+    #Keep track of spaces (doing it first to avoid problems with other matkdowns)
     text = text.replace(' ', '&nbsp;')
 
-    #add bold markdown with [b]*words*[/b]
+    # Add greentext markdown, now searching for the escaped version of >
+    text = re.sub(r'(^|\n)&gt;([^\n]*)', r'\1<span class ="greentxt">&gt;\2</span>', text)
+    
+    # Go to new line
+    text = text.replace('\n', '<br>')
+
+    # Add bold markdown with [b]*words*[/b]
     text = text.replace('[b]', '<b>').replace('[/b]', '</b>')
 
-    #add italic markdown with [i]*words*[/i]
+    # Add italic markdown with [i]*words*[/i]
     text = text.replace('[i]', '<i>').replace('[/i]', '</i>')
 
-    #add spoiler markdown with [sp]*words*[/sp]
+    # Add spoiler markdown with [sp]*words*[/sp]
     text = text.replace('[sp]', '<span class="spoiler">').replace('[/sp]', '</span>')
-
-    
 
     return text
